b15407792e06aa3d3e0b016d559062e86a34835d
AnnamPrasuna <annamprasuna15@gmail.com>
2023-12-13 12:30:26 +0530

    Add project files.

---
+++ CountryDropdowns/Page.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Page.aspx.cs" Inherits="CountryDropdowns.Page" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Select Country&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Select State&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="DropDownList2" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged">
+            </asp:DropDownList>
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Select City&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="DropDownList3" runat="server" OnSelectedIndexChanged="DropDownList3_SelectedIndexChanged" AutoPostBack="True">
+            </asp:DropDownList>
+            <br />
+            <br />
+&nbsp;&nbsp;
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            <asp:GridView ID="GridView1" runat="server" >
+            </asp:GridView>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ CountryDropdowns/Page.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace CountryDropdowns
+{
+    public partial class Page : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["hi"].ToString());
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(IsPostBack==false)
+            {
+                GetCountries();
+                GetStates();
+                GetCity();
+                FillData();
+            }
+            
+        }
+        private void GetCountries()
+        {
+            //SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["hi"].ToString());
+            string q1 = "select * from country";
+            SqlDataAdapter command=new SqlDataAdapter(q1, conn);
+            DataSet ds = new DataSet();
+            command.Fill(ds, "country");
+            DropDownList1.DataSource = ds;
+            DropDownList1.DataTextField = "countryname";
+            DropDownList1.DataValueField = "countryid";
+            DropDownList1.DataBind();
+            DropDownList1.Items.Insert(0,"--Select country---");
+        }
+        private void GetStates()
+        {
+            //SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["hi"].ToString());
+            string q2 = "select s1.stateid,s1.statename from country c1 inner join state s1 on s1.countryid=c1.countryid where c1.countryname='"+DropDownList1.SelectedItem.ToString()+"'";
+            SqlDataAdapter command = new SqlDataAdapter(q2, conn);
+            DataSet ds = new DataSet();
+            command.Fill(ds, "state");
+            DropDownList2.DataSource = ds;
+            DropDownList2.DataTextField = "statename";
+            DropDownList2.DataValueField ="stateid";
+            DropDownList2.DataBind();
+            DropDownList2.Items.Insert(0, "--Select state---");
+        }
+        private void GetCity()
+        {
+            //SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["hi"].ToString());
+            string q3 = "select c.cityid,c.cityname from state s1 inner join city c on s1.stateid=c.stateid where s1.statename='"+DropDownList2.SelectedItem.ToString()+"'";
+            SqlDataAdapter command = new SqlDataAdapter(q3, conn);
+            DataSet ds = new DataSet();
+            command.Fill(ds, "city");
+            DropDownList3.DataSource = ds;
+            DropDownList3.DataTextField ="cityname";
+            DropDownList3.DataValueField ="cityid";
+            DropDownList3.DataBind();
+            DropDownList3.Items.Insert(0, "--Select city---");
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+                GetStates();
+        }
+
+        protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           GetCity();
+        }
+
+        protected void DropDownList3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FillData();
+
+        }
+
+        protected void FillData()
+        {
+            //SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["hi"].ToString());
+            string q3 = "select e1.eno,e1.ename,e1.salary from emp e1 inner join city c1 on e1.cityid=c1.cityid where c1.cityname='" + DropDownList3.SelectedItem.ToString() + "'";
+            SqlDataAdapter command = new SqlDataAdapter(q3, conn);
+            DataSet ds = new DataSet();
+            command.Fill(ds, "city");
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+        }
+    }
+}
